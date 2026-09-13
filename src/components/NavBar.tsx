e38d59e8b069702e6d134a60e26bd742fd4895f8
Tasin <tasinhasan501@gmail.com>
2026-09-13 20:29:27 +0600

    Made the NavBar sticky

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     // console.log(navBarbtn)
 
     return (
-        <nav className="py-5 border-b border-gray-200">
+        <nav className="py-5 border-b border-gray-200 bg-white sticky top-0 z-50">
             <section className="flex justify-between items-center max-w-[80%] mx-auto">
                 <div>
                     <img src={Logo} alt="" className="hidden md:block md:w-30" />
@@ -19,11 +19,11 @@ const NavBar = () => {
                 </div>
 
                 <ul className="hidden md:flex gap-5">
-                    <li className="text-[#f53199] font-semibold">Home</li>
-                    <li className="font-light">Technologies</li>
-                    <li className="font-light">Projects</li>
-                    <li className="font-light">About</li>
-                    <li className="font-light">Contact</li>
+                    <li className="text-[#f53199] font-semibold"><a href="#">Home</a></li>
+                    <li className="font-light"><a href="#">Technologies</a></li>
+                    <li className="font-light"><a href="#">Projects</a></li>
+                    <li className="font-light"><a href="#">About</a></li>
+                    <li className="font-light"><a href="#">Contact</a></li>
                 </ul>
 
                 <div className="flex gap-5 items-center ">
